589793651e6d69f9f239e192f6f30172ac8b69f6
Matthias Loker <mmloker@gmail.com>
2014-07-30 10:56:18 -0400

    creates photo class

---
+++ app/models/photo.rb
@@ -0,0 +1,28 @@
+class Photo
+
+  attr_reader :list_of_lon_lat
+
+  def initialize(list_of_lon_lat)
+    @list_of_lon_lat = list_of_lon_lat
+  end
+
+  def sunset_by_lat_lon
+    lat, lon = *get_lat_long
+    source = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ec7bc063e59faf82689e51120e618d2d&tags=sunset&sort=interestingness-desc&lat=#{lat}&lon=#{lon}&format=json&nojsoncallback=1"
+
+    data = JSON.load(open(source))
+
+    farm_id = data["photos"]["photo"][0]["farm"]
+    server_id = data["photos"]["photo"][0]["server"]
+    id = data["photos"]["photo"][0]["id"]
+    secret = data["photos"]["photo"][0]["secret"]
+
+    "http://farm#{farm_id}.staticflickr.com/#{server_id}/#{id}_#{secret}_b.jpg"
+  end
+
+  def get_lat_long
+    list_of_lon_lat.sample
+  end
+
+end
+

40a98a405a4c11453705908da1fdcbe281da5b6d
Zack Sheppard <z@zack.io>
2014-07-31 16:01:01 -0400

    fixed index count randomizer

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -10,29 +10,39 @@ class Photo
   end
 
   def sunset_by_lat_lon
-    coordinates = self.get_lat_long
-    lat, lon = coordinates[0],coordinates[1]
-    @location = coordinates[2]
-    source = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=2a442fa279df310b1668df5f144c8f83&tags=sunset&sort=interestingness-desc&safe_search=1&lat=#{lat}&lon=#{lon}&radius=32&radius_units=km&format=json&nojsoncallback=1&extras=path_alias%2Cowner_name"
-    data = JSON.load(open(source))
-    
-    if data["photos"]["photo"].empty? || 2 > data["photos"]["photo"].count || index = nil
-      sunset_by_lat_lon
-    end
+    #binding.pry
+    if list_of_lon_lat.empty?
+      @photo_url = "http://flickr.com/47022012@N05/11917764943"
+      @author = "smileybears"
+      @location = "Atlanta"
+      "https://farm4.staticflickr.com/3846/14796102872_8d34287bf2_b.jpg"
+    else
+      coordinates = self.get_lat_long
+      lat, lon = coordinates[0],coordinates[1]
+      @location = coordinates[2]
+      source = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=2a442fa279df310b1668df5f144c8f83&tags=sunset&sort=interestingness-desc&safe_search=1&lat=#{lat}&lon=#{lon}&radius=32&radius_units=km&format=json&nojsoncallback=1&extras=path_alias%2Cowner_name"
+      data = JSON.load(open(source))
+      
+      if data["photos"]["photo"].empty? || 2 > data["photos"]["photo"].count || index = nil
+        sunset_by_lat_lon
+      end
 
-    index = rand(1..data["photos"]["photo"].count - 1 )
+      count = data["photos"]["photo"].count - 1
 
-    farm_id = data["photos"]["photo"][index]["farm"]
-    server_id = data["photos"]["photo"][index]["server"]
-    id = data["photos"]["photo"][index]["id"]
-    secret = data["photos"]["photo"][index]["secret"]
-    owner = data["photos"]["photo"][index]["owner"]
+      index = rand(1..[20, count].min )
 
-    @photo_url = "http://flikr.com/#{owner}/#{id}"
+      farm_id = data["photos"]["photo"][index]["farm"]
+      server_id = data["photos"]["photo"][index]["server"]
+      id = data["photos"]["photo"][index]["id"]
+      secret = data["photos"]["photo"][index]["secret"]
+      owner = data["photos"]["photo"][index]["owner"]
 
-    @author = data["photos"]["photo"][index]["ownername"]
+      @photo_url = "http://flickr.com/#{owner}/#{id}"
 
-    "http://farm#{farm_id}.staticflickr.com/#{server_id}/#{id}_#{secret}_b.jpg"
+      @author = data["photos"]["photo"][index]["ownername"]
+
+      "http://farm#{farm_id}.staticflickr.com/#{server_id}/#{id}_#{secret}_b.jpg"
+  end
   end
 
   def get_lat_long

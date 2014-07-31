02a6d192849c9971595c1fd252be6e75399bc226
Matthias Loker <mmloker@gmail.com>
2014-07-31 13:28:20 -0400

    intergrates refactoring of get_sunset_times

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -14,7 +14,7 @@ class Photo
     lat, lon = coordinates[0],coordinates[1]
     source = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ec7bc063e59faf82689e51120e618d2d&tags=sunset&sort=interestingness-desc&lat=#{lat}&lon=#{lon}&format=json&nojsoncallback=1"
     data = JSON.load(open(source))
-
+    
     farm_id = data["photos"]["photo"][0]["farm"]
     server_id = data["photos"]["photo"][0]["server"]
     id = data["photos"]["photo"][0]["id"]

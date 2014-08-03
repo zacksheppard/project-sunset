5e18c31a241d13736ee5bac0a6946bf669d527fc
Zack Sheppard <z@zack.io>
2014-08-02 22:05:59 -0400

    normalized lat_lon across methods

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,21 +3,21 @@ require 'json'
 
 class Photo
 
-  attr_reader :list_of_lon_lat, :author, :location, :photo_url, :lat, :lon
+  attr_reader :list_of_lat_lon, :author, :location, :photo_url, :lat, :lon
 
-  def initialize(list_of_lon_lat)
-    @list_of_lon_lat = list_of_lon_lat
+  def initialize(list_of_lat_lon)
+    @list_of_lat_lon = list_of_lat_lon
   end
 
   def sunset_by_lat_lon
     #binding.pry
-    if list_of_lon_lat.empty?
+    if list_of_lat_lon.empty?
       @photo_url = "http://flickr.com/47022012@N05/11917764943"
       @author = "smileybears"
       @location = "Atlanta"
       "https://farm4.staticflickr.com/3846/14796102872_8d34287bf2_b.jpg"
     else
-      coordinates = self.get_lat_long
+      coordinates = self.get_lat_lon
       @lat, @lon = coordinates[0],coordinates[1]
       @location = coordinates[2]
       source = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ec7bc063e59faf82689e51120e618d2d&tags=sunset&sort=interestingness-desc&safe_search=1&lat=#{@lat}&lon=#{@lon}&radius=32&radius_units=km&format=json&nojsoncallback=1&extras=path_alias%2Cowner_name%2Curl_h"
@@ -49,8 +49,8 @@ class Photo
     end
   end
 
-  def get_lat_long
-    list_of_lon_lat.sample
+  def get_lat_lon
+    list_of_lat_lon.sample
   end
 
 end
